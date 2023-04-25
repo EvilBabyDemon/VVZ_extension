7ee153b9eedd67e351af0298ce95e0c5c65f793c
EvilBabyDemon <lteufelbe@ethz.ch>
2023-04-25 17:56:19 +0200

    update links as coursereview changed

--- coursereview-links.js
+++ coursereview-links.js
@@ -3,7 +3,7 @@ function recAddUrls(e) {
         var c = element.children;
         if (c.length == 0) {
             if (element.tagName != "A") {
-                element.innerHTML = element.innerHTML.replaceAll(/(\d{3}-\d{4}-[0-9A-Z]{3})/g, "<a href=\"https://n.ethz.ch/~lteufelbe/coursereview/course/$&/\" target=\"_blank\">$&</a>");
+                element.innerHTML = element.innerHTML.replaceAll(/(\d{3}-\d{4}-[0-9A-Z]{3})/g, "<a href=\"https://n.ethz.ch/~lteufelbe/coursereview/?course=$&\" target=\"_blank\">$&</a>");
             }
         } else {
             recAddUrls(c);
