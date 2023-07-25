2240cdf3ef8c14ee3f74f492d4cc38afba6942a0
EvilBabyDemon <lteufelbe@ethz.ch>
2023-07-25 21:49:59 +0200

    asnyc function for sleep

--- on_error.js
+++ on_error.js
@@ -1,9 +1,12 @@
-//if error screen, go back
-if (window.location.href == "https://www.red9.ethz.ch/errdocs/error_000.html") {
-    var oldURL = document.referrer;
-    //only go back if vvz or vorlesungen
-    if (oldURL.includes("vvz.ethz.ch") || oldURL.includes("vorlesungen.ethz.ch")) {
-        await new Promise(r => setTimeout(r, 1500));
-        window.history.back();
+async function on_error() {
+    //if error screen, go back
+    if (window.location.href == "https://www.red9.ethz.ch/errdocs/error_000.html") {
+        var oldURL = document.referrer;
+        //only go back if vvz or vorlesungen
+        if (oldURL.includes("vvz.ethz.ch") || oldURL.includes("vorlesungen.ethz.ch")) {
+            await new Promise(r => setTimeout(r, 1500));
+            window.history.back();
+        }
     }
 }
+on_error();
