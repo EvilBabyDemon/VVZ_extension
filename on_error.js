3acd83b7d3ebb50af929d8a928fa8f104af2e4c4
EvilBabyDemon <lteufelbe@ethz.ch>
2024-12-20 14:37:45 +0100

    exponential backoff for errors

--- on_error.js
+++ on_error.js
@@ -4,7 +4,24 @@ async function on_error() {
         var oldURL = document.referrer;
         //only go back if vvz or vorlesungen
         if (oldURL.includes("vvz.ethz.ch") || oldURL.includes("vorlesungen.ethz.ch")) {
-            await new Promise(r => setTimeout(r, 1000));
+            //save errortime as [errorTime: amount of consecutive errors]
+            //consecutive errors being each less than x seconds from the last error
+            //check localstorage for last error time
+            var errorTime = new Date().getTime();
+            var lastErrorTime = localStorage.getItem("lastErrorTime");
+            var consecutiveErrors = 1;
+            if (lastErrorTime) {
+                [lastErrorTime, consecutiveErrors] = lastErrorTime.split(",");
+                //last error was less than 15 seconds ago
+                if (errorTime - lastErrorTime < 1000 * 15) {
+                    consecutiveErrors++;
+                } else {
+                    consecutiveErrors = 1;
+                }
+            }
+            localStorage.setItem("lastErrorTime", errorTime + "," + consecutiveErrors);
+            var timeout = 1000 * consecutiveErrors;
+            await new Promise(r => setTimeout(r, timeout));
             window.history.back();
         }
     }
